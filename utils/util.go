03014f211367cf8d78c414a8932f65d6b538e53a
here-bossnanda <m.trinandanoviardy@gmail.com>
2023-03-21 23:18:35 +0700

    [nanda] setting router

---
+++ utils/util.go
@@ -0,0 +1,34 @@
+package utils
+
+import (
+	"encoding/json"
+	"log"
+	"reflect"
+)
+
+func PrintLog(err interface{}) {
+	if err != nil {
+		log.Print(err)
+	}
+}
+
+func IsNil(val interface{}) bool {
+	if val == nil {
+		return true
+	}
+	switch reflect.TypeOf(val).Kind() {
+	case reflect.Ptr, reflect.Map, reflect.Array, reflect.Chan:
+		//use of IsNil method
+		return reflect.ValueOf(val).IsNil()
+	}
+	if reflect.ValueOf(val).Kind() != reflect.Ptr && reflect.ValueOf(val).Len() == 0 {
+		return true
+	}
+	return false
+}
+
+func AutoMap(from interface{}, to interface{}) error {
+	jsonFrom, _ := json.Marshal(from)
+	err := json.Unmarshal([]byte(string(jsonFrom)), to)
+	return err
+}
